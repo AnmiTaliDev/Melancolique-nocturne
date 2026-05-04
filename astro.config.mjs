23642a2932cb82d833f009e2b6a012dd44ec5d5a
AnmiTaliDev <anmitali198@gmail.com>
2026-05-04 10:25:08 +0500

    feat: migrate to Astro, translate to English, redesign UI

---
+++ astro.config.mjs
@@ -0,0 +1,5 @@
+// SPDX-FileCopyrightText: AnmiTaliDev <anmitalidev@nuros.org>
+// SPDX-License-Identifier: AGPL-3.0-only
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({});
